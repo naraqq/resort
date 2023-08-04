aebcdc071ed5f3afdd0f7d49e2d2854f638b2fb8
narmandakh.g <narmandakh.g@ddishtv.mn>
2023-08-04 18:27:25 +0800

    update

--- src/app/components/Welcome.jsx
+++ src/app/components/Welcome.jsx
@@ -12,15 +12,15 @@ function Welcome() {
         <div className="cover-overlay"></div>{" "}
       </div>
       <div className="absolute top-[calc(35%)] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-white text-lg md:text-2xl text-center">
+        {/* <h1 className="text-white text-lg md:text-2xl text-center">
           Тавтай морил
-        </h1>
-        <h1 className="text-white text-4xl md:text-8xl mt-10 uppercase text-center">
+        </h1> */}
+        <h1 className="text-white text-4xl md:text-8xl mt-10 uppercase text-center font-light">
           Үүрэг ресорт
         </h1>
       </div>
       <div className="z-10 absolute top-[calc(75%)] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <button className=" hover:!ring-[1px] text-center !px-12 !py-5 border  ring-gray-200 font-light">
+        <button className=" hover:!ring-[1px] text-center !px-12 !py-4 border  ring-gray-200 font-light">
           <Link
             className="text-white no-underline text-[15px] select-none uppercase "
             href="#about"
